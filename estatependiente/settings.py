a20a51c3c120c44b6a625c7ef44ff86a9c4caa4b
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-10-20 18:37:03 +0200

    Added settings, .gitignore and README file and new path to products app

--- estatependiente/settings.py
+++ estatependiente/settings.py
@@ -1,7 +1,10 @@
 # Django settings for estatependiente project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+CURRENT_PATH=os.path.dirname(__file__)
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -11,13 +14,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': os.getenv('ESTATEPENDIENTE_ENGINE','django.db.backends.sqlite3'), # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.getenv('ESTATEPENDIENTE_DB','shopfront.db'),                      # Or path to database file if using sqlite3.
+        'USER': os.getenv('ESTATEPENDIENTE_USERDB',''),                      # Not used with sqlite3.
+        'PASSWORD': os.getenv('ESTATEPENDIENTE_PASSDB',''),                  # Not used with sqlite3.
+        'HOST': os.getenv('ESTATEPENDIENTE_HOSTDB',''),                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': os.getenv('ESTATEPENDIENTE_PORTDB',''),                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -29,11 +31,11 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Madrid'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 SITE_ID = 1
 
@@ -50,12 +52,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.getenv('ESTATEPENDIENTE_MEDIA',os.path.join(CURRENT_PATH,'media'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -72,6 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.getenv('ESTATEPENDIENTE_STATIC',os.path.join(CURRENT_PATH,'static')),
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +114,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.getenv('ESTATEPENDIENTE_TEMPLATES',os.path.join(CURRENT_PATH,'templates')),
 )
 
 INSTALLED_APPS = (
@@ -120,10 +124,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    #Import Ajax app
+    'dajaxice',
+    #Aplicacion
+    'estatependiente.products',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
@@ -146,7 +151,17 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'file':{
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': os.getenv('ESTATEPENDIENTE_INFOLOG','info.log'),
+        },
+        'file':{
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.getenv('ESTATEPENDIENTE_ERRORLOG','error.log'),
+        },
     },
     'loggers': {
         'django.request': {
