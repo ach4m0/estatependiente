1a92f636ef7a54424d454271c806c622914266bd
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-11-03 11:52:50 +0100

    Category view and template

---
+++ estatependiente/products/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+from .models import Category,Product
+#from estatependiente.parameters.models import Parameter
+
+def view_category(request,category):
+    categories = Category.objects.all()
+    products = Product.objects.filter(category__path=category)
+    subcategories = products.values('subcategory__name','subcategory__path').distinct()
+#    parameters = Parameter.objects.all()
+    return render_to_response('products.html',
+                            {'categories': categories,
+                            'products': products,
+                            'subcategories': subcategories,
+#                            'contact': parameters.get(name='contacto')
+                            },context_instance=RequestContext(request))
--- estatependiente/urls.py
+++ estatependiente/urls.py
@@ -8,7 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'estatependiente.views.home', name='home'),
-#    url(r'^categoria/(?P<category>\w+)/$','estatependiente.products.views.view_category',name='view_category'),
+    url(r'^categoria/(?P<category>\w+)/$','estatependiente.products.views.view_category',name='view_category'),
 #    url(r'^producto/(?P<producto>\w+)/$','estatependiente.products.views.view_product',name='view_product'),
 #    #Admin url
     url(r'^admin/', include(admin.site.urls)),
