143675b60569c5b00af573c4ac7d43c7ee295cbc
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-11-03 12:11:28 +0100

    End comment to parameters vars

--- estatependiente/views.py
+++ estatependiente/views.py
@@ -5,18 +5,18 @@ from django.template.context import RequestContext
 from django.http import HttpResponseRedirect
 
 from estatependiente.products.models import Category,Product
-#from estatependiente.parameters.models import Parameter
+from estatependiente.parameters.models import Parameter
 
 def home(request):
     categories = Category.objects.all()
     products = Product.objects.all()[:3]
-#    parameters = Parameter.objects.all()
-#    welcome = parameters.get(name='bienvenida')
-#    contact = parameters.get(name='contacto')
+    parameters = Parameter.objects.all()
+    welcome = parameters.get(name='bienvenida')
+    contact = parameters.get(name='contacto')
     return render_to_response('home.html',
                             {'categories': categories,
                             'products': products,
-#                            'welcome': welcome,
-#                            'contact': contact,
+                            'welcome': welcome,
+                            'contact': contact,
                             },context_instance=RequestContext(request))
 
