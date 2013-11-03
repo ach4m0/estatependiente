96a35b061fd1a18ef248cd21ff38ece36e2c8dc9
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-11-03 12:12:33 +0100

    Updated products views and base template

--- estatependiente/products/views.py
+++ estatependiente/products/views.py
@@ -8,10 +8,10 @@ def view_category(request,category):
     categories = Category.objects.all()
     products = Product.objects.filter(category__path=category)
     subcategories = products.values('subcategory__name','subcategory__path').distinct()
-#    parameters = Parameter.objects.all()
+    parameters = Parameter.objects.all()
     return render_to_response('products.html',
                             {'categories': categories,
                             'products': products,
                             'subcategories': subcategories,
-#                            'contact': parameters.get(name='contacto')
+                            'contact': parameters.get(name='contacto')
                             },context_instance=RequestContext(request))
