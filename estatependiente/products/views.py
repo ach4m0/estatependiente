312074bedd37b005d041271e2f1b2b71008adad2
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-12-01 10:01:57 +0100

    New view to product detail

--- estatependiente/products/views.py
+++ estatependiente/products/views.py
@@ -15,3 +15,14 @@ def view_category(request,category):
                             'subcategories': subcategories,
                             'contact': parameters.get(name='contacto')
                             },context_instance=RequestContext(request))
+
+def view_product(request,product):
+    categories = Category.objects.all()
+    pr = Product.objects.get(path=product)
+    parameters = Parameter.objects.all()
+    return render_to_response('product.html',
+                            {'categories': categories,
+                            'product': pr,
+                            'contact': parameters.get(name='contacto')
+                            },context_instance=RequestContext(request))
+
